82f531fa1e83a08a408b1fc7baa1d267686749ce
Patrick Beagan <pbeagan1@gmail.com>
2022-02-04 23:38:25 -0600

    adding in a basic form for submitting images

--- app/src/main/kotlin/dev/patbeagan/consolevision/server/routes/GetHome.kt
+++ app/src/main/kotlin/dev/patbeagan/consolevision/server/routes/GetHome.kt
@@ -1,33 +1,43 @@
 package dev.patbeagan.consolevision.server.routes
 
-import dev.patbeagan.consolevision.ConsoleVisionRuntime
-import dev.patbeagan.consolevision.ImageScaler
 import dev.patbeagan.consolevision.server.RouteHandler
 import io.ktor.application.ApplicationCall
-import io.ktor.response.respondText
-import java.awt.image.BufferedImage
-import java.net.URL
-import javax.imageio.ImageIO
+import io.ktor.html.respondHtml
+import kotlinx.html.FormEncType
+import kotlinx.html.FormMethod
+import kotlinx.html.InputType
+import kotlinx.html.body
+import kotlinx.html.br
+import kotlinx.html.form
+import kotlinx.html.h1
+import kotlinx.html.h3
+import kotlinx.html.id
+import kotlinx.html.input
+import kotlinx.html.label
 
 class GetHome : RouteHandler {
     override suspend fun handle(call: ApplicationCall) {
-        val url = URL(
-            "https://www.freepsdbazaar.com/wp-content/uploads/2020/06/sky-replace/sky-sunset/sunset-050-freepsdbazaar.jpg"
-        )
-        val file: BufferedImage = ImageIO.read(url)
-        val scaledImage = ImageScaler(width = 80, height = 80).scaledImage(file)
-        if (scaledImage != null) {
-            val printFrame = ConsoleVisionRuntime(
-                paletteImage = null,
-                reductionRate = 0,
-                paletteReductionRate = 0,
-                isCompatPalette = false,
-                shouldNormalize = false,
-            ).printFrame(scaledImage)
-
-            printFrame.let { call.respondText(it) }
-        } else {
-            call.respondText("Could not process the image.")
+        call.respondHtml {
+            body {
+                h1 {
+                    +"show file select"
+                }
+                h3 {
+                    +"Show a file-select field which allows only one file to be chosen:"
+                }
+                form(action = "/upload", method = FormMethod.post, encType = FormEncType.multipartFormData) {
+                    label {
+                        htmlFor = "myFile"
+                        +"Select a file"
+                    }
+                    input(type = InputType.file) {
+                        id = "myFile"
+                        name = "myFile"
+                    }
+                    br { }
+                    input(type = InputType.submit)
+                }
+            }
         }
     }
 }
